1422951ce78794574aa8008369d76fac702462eb
Dmitriy Krasnikov <dmikov@gmail.com>
2016-05-31 10:34:26 -0400

    Initial commit
    Compilable, but not wired to generate

--- src/NSwag/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NSwag.Console")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Rico Suter")]
-[assembly: AssemblyProduct("NSwag.Console")]
-[assembly: AssemblyCopyright("Copyright © Rico Suter, 2015")]
-[assembly: AssemblyVersion("2.36.*")]
