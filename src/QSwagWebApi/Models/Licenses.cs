9636f20226ff3211dc6b621cadabc0c40e5f9dcc
Dmitriy Krasnikov <dmikov@gmail.com>
2017-03-21 10:07:50 -0400

    Missing file

---
+++ src/QSwagWebApi/Models/Licenses.cs
@@ -0,0 +1,7 @@
+﻿namespace QSwagWebApi.Models
+{
+    public class Licenses
+    {
+        public string Newtonsoft { get; set; } = "Put your license here or in appsettings.private.json";
+    }
+}
