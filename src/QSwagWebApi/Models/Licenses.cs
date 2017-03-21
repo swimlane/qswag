f7838b6731e5ea673ef5a66bba2f90909aa97f49
Dmitriy Krasnikov <dmikov@gmail.com>
2017-03-21 10:08:09 -0400

    Comments

--- src/QSwagWebApi/Models/Licenses.cs
+++ src/QSwagWebApi/Models/Licenses.cs
@@ -1,7 +1,16 @@
 ﻿namespace QSwagWebApi.Models
 {
+    /// <summary>
+    /// License provider.
+    /// </summary>
     public class Licenses
     {
+        /// <summary>
+        /// Gets or sets the newtonsoft license.
+        /// </summary>
+        /// <value>
+        /// The newtonsoft license.
+        /// </value>
         public string Newtonsoft { get; set; } = "Put your license here or in appsettings.private.json";
     }
 }
