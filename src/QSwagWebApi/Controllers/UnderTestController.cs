3d2a2f8f64a8fba8568bce889ee69582dcec9c2c
Dmitriy Krasnikov <dmikov@gmail.com>
2017-08-17 14:03:37 -0400

    Fixed it back, was wrong logically original should have been per method, not per route

--- src/QSwagWebApi/Controllers/UnderTestController.cs
+++ src/QSwagWebApi/Controllers/UnderTestController.cs
@@ -1,29 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using QSwagGenerator.Annotations;
 
 namespace QSwagWebApi.Controllers
 {
+    public class NameValue
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
     /// <summary>
-    /// Different types of routes controller.
+    /// Credentials provider. With encrypted data at rest.
     /// </summary>
     /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
-    public class UnderTestController : Controller
+    public class UnderTest : Controller
     {
         /// <summary>
-        /// Imports application (and surrounding entities).
+        /// Gets the credentials.
         /// </summary>
-        /// <param name="id">The root application identifier.</param>
-        /// <param name="json">The import json.</param>
-        /// <returns>
-        /// Collection of imprted applications: id and name.
-        /// </returns>
-        [Tag("application")]
-        [HttpPost, Route("app/{id?}/import")]
-        public void ApplicationImport(string id, [FromBody] dynamic json)
+        /// <returns>Dictionary of credential names with masked values.</returns>
+        [HttpGet, Route("credentials")]
+        public Dictionary<string, string> GetCredentials()
         {
-            throw new NotImplementedException();
+            return null;
+        }
+        /// <summary>
+        /// Gets the credential by name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>Name value pair (value is masked)</returns>
+        [HttpGet, Route("credentials/{name}")]
+        public NameValue GetCredential(string name)
+        { 
+            return new NameValue() { Name = name };
+        }
+        /// <summary>
+        /// Creates new credentials.
+        /// </summary>
+        /// <param name="nameValue">The credential key value pair.</param>
+        /// <returns>Unencrypted credentials back.</returns>
+        [HttpPost, Route("credentials")]
+        public IActionResult CreateCredentials([FromBody] NameValue nameValue)
+        {
+           return (IActionResult)Ok(nameValue);
+        }
+
+        /// <summary>
+        /// Updates the credentials.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="name">The name.</param>
+        /// <returns>Microsoft.AspNetCore.Mvc.IActionResult.</returns>
+        [HttpPut, Route("credentials/{name?}")]
+        public IActionResult UpdateCredentials([FromBody] NameValue entity, [FromRoute] string name)
+        {
+            return (IActionResult)Ok(entity);
+        }
+
+
+        /// <summary>
+        /// Deletes the credentials.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>Microsoft.AspNetCore.Mvc.IActionResult.</returns>
+        [HttpDelete, Route("credentials/{name}")]
+        public IActionResult DeleteCredentials(string name)
+        {
+           return  (IActionResult)Ok();
         }
     }
 }
